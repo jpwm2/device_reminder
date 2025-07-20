72da86413e521407de26571074783e4d29741506
jpwm2 <118033153+jpwm2@users.noreply.github.com>
2025-07-20 16:34:00 +0900

    Implement dependency injection

--- di/app_injector.hpp
+++ di/app_injector.hpp
@@ -1,7 +1,18 @@
 #pragma once
 
 #include "di/logger_injector.hpp"
-
+#include "core/main_task/main_task.hpp"
+#include "core/human_task/human_task.hpp"
+#include "core/bluetooth_task/bluetooth_task.hpp"
+#include "core/buzzer_task/buzzer_task.hpp"
+#include "infra/timer_service/timer_service.hpp"
+#include "infra/pir_driver/pir_driver.hpp"
+#include "infra/gpio_driver/gpio_driver.hpp"
+#include "infra/buzzer_driver/buzzer_driver.hpp"
+#include "infra/bluetooth_driver/bluetooth_driver.hpp"
+#include "infra/message_operator/message_sender.hpp"
+#include "infra/message_operator/local_message_queue.hpp"
+#include "infra/message_operator/i_message_queue.hpp"
 
 #include <tuple>
 #include <boost/di.hpp>
@@ -26,9 +37,32 @@ auto merge_injectors(Tuples&&... tuples) {
 
 inline auto make_app_injector() {
     auto logger = make_logger_injector();
+    // BluetoothScanner の実装が存在しないため、簡易スキャナを定義
+    struct DummyBluetoothScanner : IBluetoothScanner {
+        std::vector<AdvertisementInfo> scan() override { return {}; }
+    };
+
+    auto core = std::tuple{
+        di::bind<IMainTask>.to<MainTask>(),
+        di::bind<IHumanTask>.to<HumanTask>(),
+        di::bind<IBluetoothTask>.to<BluetoothTask>(),
+        di::bind<IBuzzerTask>.to<BuzzerTask>(),
+        di::bind<ITimerService>.to<TimerService>(),
+        di::bind<IMessageQueue>.to<LocalMessageQueue>(),
+        di::bind<IMessageSender>.to([](const std::shared_ptr<ILogger>& lg) {
+            static int id = 0;
+            return std::make_shared<MessageSender>("/devreminder_" + std::to_string(id++), 10, lg);
+        }),
+        di::bind<IGPIODriver>.to<GPIODriver>(),
+        di::bind<IBuzzerDriver>.to<BuzzerDriver>(),
+        di::bind<IPIRDriver>.to<PIRDriver>(),
+        di::bind<IBluetoothScanner>.to<DummyBluetoothScanner>(),
+        di::bind<IBluetoothDriver>.to<BluetoothDriver>()
+    };
 
     return detail::merge_injectors(
-        logger
+        logger,
+        core
     );
 }
 
--- tests/core/test_buzzer_task.cpp
+++ tests/core/test_buzzer_task.cpp
@@ -11,8 +11,8 @@ namespace device_reminder {
 
 class MockDriver : public IBuzzerDriver {
 public:
-    MOCK_METHOD(void, start_buzzer, (), (override));
-    MOCK_METHOD(void, stop_buzzer, (), (override));
+    MOCK_METHOD(bool, start, (), (override));
+    MOCK_METHOD(bool, stop, (), (override));
 };
 
 class MockTimer : public ITimerService {
@@ -35,12 +35,12 @@ TEST(BuzzerTaskTest, StartAndTimeoutStopsBuzzer) {
 
     BuzzerTask task(driver, timer, logger);
 
-    EXPECT_CALL(*driver, start_buzzer());
+    EXPECT_CALL(*driver, start());
     EXPECT_CALL(*timer, start(4000, testing::Field(&Message::type_, MessageType::Timeout)));
     task.send_message(Message{MessageType::BuzzerOn});
     EXPECT_EQ(task.state(), BuzzerTask::State::Buzzing);
 
-    EXPECT_CALL(*driver, stop_buzzer());
+    EXPECT_CALL(*driver, stop());
     EXPECT_CALL(*timer, stop()).Times(0);
     task.send_message(Message{MessageType::Timeout});
     EXPECT_EQ(task.state(), BuzzerTask::State::WaitStart);
@@ -53,11 +53,11 @@ TEST(BuzzerTaskTest, ManualStopCancelsTimer) {
 
     BuzzerTask task(driver, timer, logger);
 
-    EXPECT_CALL(*driver, start_buzzer());
+    EXPECT_CALL(*driver, start());
     EXPECT_CALL(*timer, start(4000, testing::Field(&Message::type_, MessageType::Timeout)));
     task.send_message(Message{MessageType::BuzzerOn});
 
-    EXPECT_CALL(*driver, stop_buzzer());
+    EXPECT_CALL(*driver, stop());
     EXPECT_CALL(*timer, stop());
     task.send_message(Message{MessageType::BuzzerOff});
     EXPECT_EQ(task.state(), BuzzerTask::State::WaitStart);
@@ -70,7 +70,7 @@ TEST(BuzzerTaskTest, IgnoreDuplicateStart) {
 
     BuzzerTask task(driver, timer, logger);
 
-    EXPECT_CALL(*driver, start_buzzer());
+    EXPECT_CALL(*driver, start());
     EXPECT_CALL(*timer, start(4000, testing::Field(&Message::type_, MessageType::Timeout)));
     task.send_message(Message{MessageType::BuzzerOn});
     EXPECT_EQ(task.state(), BuzzerTask::State::Buzzing);
