d8e9a4ece98ce6228ccafc9ffa882807be6441a5
jpwm2 <118033153+jpwm2@users.noreply.github.com>
2025-07-27 19:29:49 +0900

    Implement ThreadReceiver interface and class

---
+++ include/infra/thread_operation/thread_receiver/thread_receiver.hpp
@@ -0,0 +1,28 @@
+#pragma once
+#include "infra/thread_operation/thread_receiver/i_thread_receiver.hpp"
+#include "infra/thread_operation/thread_queue/i_thread_queue.hpp"
+#include "infra/thread_operation/thread_message/thread_message.hpp"
+#include "infra/thread_operation/thread_dispatcher/i_thread_dispatcher.hpp"
+#include "infra/logger/i_logger.hpp"
+#include <memory>
+#include <atomic>
+
+namespace device_reminder {
+
+class ThreadReceiver : public IThreadReceiver {
+public:
+    ThreadReceiver(std::shared_ptr<IThreadQueue<ThreadMessage>> queue,
+                   std::shared_ptr<IThreadDispatcher> dispatcher,
+                   std::shared_ptr<ILogger> logger = nullptr);
+
+    void run() override;
+    void stop() override;
+
+private:
+    std::shared_ptr<IThreadQueue<ThreadMessage>> queue_;
+    std::shared_ptr<IThreadDispatcher> dispatcher_;
+    std::atomic<bool> running_{true};
+    std::shared_ptr<ILogger> logger_;
+};
+
+} // namespace device_reminder
