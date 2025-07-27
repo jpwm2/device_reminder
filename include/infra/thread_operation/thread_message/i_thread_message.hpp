529a63351896d41d0d2975e212577567e6be5a9a
jpwm2 <118033153+jpwm2@users.noreply.github.com>
2025-07-27 16:31:00 +0900

    Add ThreadMessage interfaces

---
+++ include/infra/thread_operation/thread_message/i_thread_message.hpp
@@ -0,0 +1,24 @@
+#pragma once
+#include <memory>
+#include <string>
+
+namespace device_reminder {
+
+enum class ThreadMessageType {
+    HumanDetected,
+    StartBuzzer,
+    StopBuzzer,
+    Timeout,
+    BluetoothScanRequested,
+    BluetoothScanResponse
+};
+
+class IThreadMessage {
+public:
+    virtual ~IThreadMessage() = default;
+    virtual ThreadMessageType type() const noexcept = 0;
+    virtual std::shared_ptr<IThreadMessage> clone() const = 0;
+    virtual std::string to_string() const = 0;
+};
+
+} // namespace device_reminder
