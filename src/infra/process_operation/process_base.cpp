f2312b3746562c9fd5370e70d0dd41b9d6a469d2
jpwm2 <118033153+jpwm2@users.noreply.github.com>
2025-07-28 12:35:48 +0900

    Update ProcessBase to new spec

--- src/infra/process_operation/process_base.cpp
+++ src/infra/process_operation/process_base.cpp
@@ -2,25 +2,31 @@
 #include <csignal>
 #include <thread>
 
-#include "process/process_base.hpp"
-#include "process_message_operation/i_process_message_receiver.hpp"
-#include "process_message_operation/i_process_message_sender.hpp"
-#include "worker_dispatcher/i_worker_dispatcher.hpp"
+#include "infra/process_operation/process_base/process_base.hpp"
+#include "infra/process_operation/process_queue/i_process_queue.hpp"
+#include "infra/process_operation/process_receiver/i_process_receiver.hpp"
+#include "infra/process_operation/process_sender/i_process_sender.hpp"
+#include "infra/process_operation/process_dispatcher/i_process_dispatcher.hpp"
+#include "infra/io/i_file_loader.hpp"
 #include "infra/logger/i_logger.hpp"
-#include <functional>
 
 std::atomic<bool> ProcessBase::g_stop_flag{false};
 
-ProcessBase::ProcessBase(std::shared_ptr<IProcessMessageReceiver> receiver,
-                         std::shared_ptr<IWorkerDispatcher> dispatcher,
-                         std::shared_ptr<IProcessMessageSender> sender,
-                         std::function<int()> load_setting_value,
-                         std::shared_ptr<ILogger> logger)
-    : receiver_(std::move(receiver))
+ProcessBase::ProcessBase(std::shared_ptr<IProcessMessageQueue>    queue,
+                         std::shared_ptr<IProcessMessageReceiver> receiver,
+                         std::shared_ptr<IWorkerDispatcher>       dispatcher,
+                         std::shared_ptr<IProcessMessageSender>   sender,
+                         std::shared_ptr<IFileLoader>             file_loader,
+                         std::shared_ptr<ILogger>                 logger,
+                         std::string                              process_name)
+    : queue_(std::move(queue))
+    , receiver_(std::move(receiver))
     , dispatcher_(std::move(dispatcher))
     , sender_(std::move(sender))
-    , load_setting_value_(std::move(load_setting_value))
+    , file_loader_(std::move(file_loader))
     , logger_(std::move(logger))
+    , process_name_(std::move(process_name))
+    , priority_{file_loader_ ? file_loader_->load_int("priority") : 0}
 
 {
     // Ctrl‑C (SIGINT) を捕捉して終了フラグを立てる
@@ -58,6 +64,5 @@ void ProcessBase::stop()
 
 int ProcessBase::priority() const noexcept
 {
-    if (load_setting_value_) return load_setting_value_();
-    return 0;
+    return priority_;
 }
