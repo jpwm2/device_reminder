b579877b4fdb16fca42a54f6c7fdd1c98f9a53a9
jpwm2 <118033153+jpwm2@users.noreply.github.com>
2025-07-30 13:01:30 +0900

    Fix ThreadReceiver to use non-templated queue

--- include/infra/thread_operation/thread_receiver/thread_receiver.hpp
+++ include/infra/thread_operation/thread_receiver/thread_receiver.hpp
@@ -1,7 +1,7 @@
 #pragma once
 #include "infra/thread_operation/thread_receiver/i_thread_receiver.hpp"
 #include "infra/thread_operation/thread_queue/i_thread_queue.hpp"
-#include "infra/thread_operation/thread_message/thread_message.hpp"
+#include "infra/thread_operation/thread_message/i_thread_message.hpp"
 #include "infra/thread_operation/thread_dispatcher/i_thread_dispatcher.hpp"
 #include "infra/logger/i_logger.hpp"
 #include <memory>
@@ -11,7 +11,7 @@ namespace device_reminder {
 
 class ThreadReceiver : public IThreadReceiver {
 public:
-    ThreadReceiver(std::shared_ptr<IThreadQueue<ThreadMessage>> queue,
+    ThreadReceiver(std::shared_ptr<IThreadQueue> queue,
                    std::shared_ptr<IThreadDispatcher> dispatcher,
                    std::shared_ptr<ILogger> logger = nullptr);
 
@@ -19,7 +19,7 @@ public:
     void stop() override;
 
 private:
-    std::shared_ptr<IThreadQueue<ThreadMessage>> queue_;
+    std::shared_ptr<IThreadQueue> queue_;
     std::shared_ptr<IThreadDispatcher> dispatcher_;
     std::atomic<bool> running_{true};
     std::shared_ptr<ILogger> logger_;
