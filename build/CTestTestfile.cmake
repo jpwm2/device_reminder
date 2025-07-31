d8a413443be27eb417a22fcd68d62bdfc9acd3b0
jpwm2 <118033153+jpwm2@users.noreply.github.com>
2025-07-31 09:05:43 +0900

    Remove build directory

--- build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/kohsaku/Library/CloudStorage/OneDrive-個人用/Documents/workspace/ローカル開発/device_reminder
-# Build directory: /Users/kohsaku/Library/CloudStorage/OneDrive-個人用/Documents/workspace/ローカル開発/device_reminder/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(AllTests "/Users/kohsaku/Library/CloudStorage/OneDrive-個人用/Documents/workspace/ローカル開発/device_reminder/build/test_app")
-set_tests_properties(AllTests PROPERTIES  _BACKTRACE_TRIPLES "/Users/kohsaku/Library/CloudStorage/OneDrive-個人用/Documents/workspace/ローカル開発/device_reminder/CMakeLists.txt;38;add_test;/Users/kohsaku/Library/CloudStorage/OneDrive-個人用/Documents/workspace/ローカル開発/device_reminder/CMakeLists.txt;0;")
-subdirs("external/googletest")
--- build/external/googletest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/kohsaku/Library/CloudStorage/OneDrive-個人用/Documents/workspace/ローカル開発/device_reminder/external/googletest/googletest
-# Build directory: /Users/kohsaku/Library/CloudStorage/OneDrive-個人用/Documents/workspace/ローカル開発/device_reminder/build/external/googletest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
