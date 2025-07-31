8a231ad2fd73bb0ce1856a4b6fc1258dc0ca0b5b
jpwm2 <118033153+jpwm2@users.noreply.github.com>
2025-07-31 12:16:52 +0900

    Add HumanProcess skeleton

---
+++ include/core/human_task/human_process.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "core/human_task/i_human_process.hpp"
+#include "infra/process_operation/process_base/process_base.hpp"
+#include "infra/watch_dog/i_watch_dog.hpp"
+#include "core/interfaces/i_handler.hpp"
+#include "core/human_task/i_human_task.hpp"
+
+#include <memory>
+
+namespace device_reminder {
+
+class HumanProcess : public ProcessBase, public IHumanProcess {
+public:
+    HumanProcess(std::shared_ptr<IProcessQueue>    queue,
+                 std::shared_ptr<IProcessReceiver> receiver,
+                 std::shared_ptr<IProcessDispatcher> dispatcher,
+                 std::shared_ptr<IProcessSender>   sender,
+                 std::shared_ptr<IFileLoader>      file_loader,
+                 std::shared_ptr<ILogger>          logger,
+                 std::shared_ptr<IWatchDog>        watch_dog,
+                 std::shared_ptr<IHandler>         handler,
+                 std::shared_ptr<IHumanTask>       task);
+
+    void run() override;
+    void stop() override;
+
+private:
+    std::shared_ptr<IHumanTask> human_task_;
+    std::shared_ptr<IHandler>   handler_;
+    std::shared_ptr<IWatchDog>  watch_dog_;
+};
+
+} // namespace device_reminder
---
+++ src/core/human_task/human_process.cpp
@@ -0,0 +1,40 @@
+#include "human_task/human_process.hpp"
+#include "infra/logger/i_logger.hpp"
+
+namespace device_reminder {
+
+HumanProcess::HumanProcess(std::shared_ptr<IProcessQueue>    queue,
+                           std::shared_ptr<IProcessReceiver> receiver,
+                           std::shared_ptr<IProcessDispatcher> dispatcher,
+                           std::shared_ptr<IProcessSender>   sender,
+                           std::shared_ptr<IFileLoader>      file_loader,
+                           std::shared_ptr<ILogger>          logger,
+                           std::shared_ptr<IWatchDog>        watch_dog,
+                           std::shared_ptr<IHandler>         handler,
+                           std::shared_ptr<IHumanTask>       task)
+    : ProcessBase(std::move(queue),
+                  std::move(receiver),
+                  std::move(dispatcher),
+                  std::move(sender),
+                  std::move(file_loader),
+                  std::move(logger),
+                  "HumanProcess")
+    , human_task_(std::move(task))
+    , handler_(std::move(handler))
+    , watch_dog_(std::move(watch_dog))
+{
+    if (logger_) logger_->info("HumanProcess created");
+}
+
+void HumanProcess::run() {
+    if (watch_dog_) watch_dog_->start();
+    ProcessBase::run();
+    if (watch_dog_) watch_dog_->stop();
+}
+
+void HumanProcess::stop() {
+    if (watch_dog_) watch_dog_->stop();
+    ProcessBase::stop();
+}
+
+} // namespace device_reminder
