eb73d1783fd1500992ec6575b944204990e1a1ca
jpwm2 <118033153+jpwm2@users.noreply.github.com>
2025-07-31 14:06:02 +0900

    Update setup.sh

--- run/setup.sh
+++ run/setup.sh
@@ -10,39 +10,42 @@ echo -e "\e[0m"
 
 # 基本パッケージのインストール
 echo -e "\e[1;34m[INFO]\e[0m 必要なパッケージをインストール中..."
-sudo apt-get update
-sudo apt-get install -y build-essential cmake gdb valgrind libgpiod-dev git
+if sudo apt-get update && sudo apt-get install -y build-essential cmake gdb valgrind libgpiod-dev git; then
+  echo -e "\e[1;32m[SUCCESS]\e[0m パッケージのインストール完了"
+else
+  echo -e "\e[1;31m[FAILED]\e[0m パッケージのインストールに失敗しました" >&2
+  exit 1
+fi
 
 # 外部依存を手動で取得
-echo -e "\e[1;34m[INFO]\e[0m 外部ライブラリを取得中..."
+echo -e "\n\e[1;34m[INFO]\e[0m 外部ライブラリを取得中..."
 mkdir -p external
 cd external
 
-# googletest
-if [ ! -d googletest ]; then
-  echo -e "\e[1;32m[CLONE]\e[0m googletest をクローン中..."
-  git clone https://github.com/google/googletest.git
-else
-  echo -e "\e[1;36m[SKIP]\e[0m googletest は既に存在します"
-fi
+# リポジトリクローン関数
+clone_repo() {
+  local name=$1
+  local url=$2
 
-# spdlog
-if [ ! -d spdlog ]; then
-  echo -e "\e[1;32m[CLONE]\e[0m spdlog をクローン中..."
-  git clone https://github.com/gabime/spdlog.git
-else
-  echo -e "\e[1;36m[SKIP]\e[0m spdlog は既に存在します"
-fi
+  if [ ! -d "$name" ]; then
+    echo -e "\e[1;32m[CLONE]\e[0m $name をクローン中..."
+    if git clone "$url" "$name"; then
+      echo -e "\e[1;32m[SUCCESS]\e[0m $name のクローン成功"
+    else
+      echo -e "\e[1;31m[FAILED]\e[0m $name のクローンに失敗しました" >&2
+      exit 1
+    fi
+  else
+    echo -e "\e[1;36m[SKIP]\e[0m $name は既に存在します"
+  fi
+}
 
-# boost.di
-if [ ! -d di ]; then
-  echo -e "\e[1;32m[CLONE]\e[0m boost.di をクローン中..."
-  git clone https://github.com/boost-ext/di.git
-else
-  echo -e "\e[1;36m[SKIP]\e[0m boost.di は既に存在します"
-fi
+# 各ライブラリの取得
+clone_repo googletest https://github.com/google/googletest.git
+clone_repo spdlog https://github.com/gabime/spdlog.git
+clone_repo di https://github.com/boost-ext/di.git
 
 cd ..
 
 # 完了メッセージ
-echo -e "\e[1;32m✅ 環境セットアップが完了しました！\e[0m"
+echo -e "\n\e[1;32m✅ 環境セットアップが正常に完了しました！\e[0m"
