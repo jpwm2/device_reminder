6b37177d6620278d01b05d78c97f47062b44386d
jpwm2 <118033153+jpwm2@users.noreply.github.com>
2025-08-01 09:49:43 +0900

    Update TimerService test after directory change

--- tests/infra/test_timer_service.cpp
+++ tests/infra/test_timer_service.cpp
@@ -2,21 +2,22 @@
 #include <gmock/gmock.h>
 
 #include "infra/timer_service/timer_service.hpp"
-#include "process_message_operation/i_process_message_sender.hpp"
-#include "process_message_operation/process_message.hpp"
-#include <thread>
+#include "infra/thread_operation/thread_sender/i_thread_sender.hpp"
+#include "infra/logger/i_logger.hpp"
+
 #include <chrono>
+#include <thread>
 
 using namespace device_reminder;
 using ::testing::StrictMock;
 using ::testing::NiceMock;
 
 namespace {
-class MockSender : public IProcessMessageSender {
+class MockSender : public IThreadSender {
 public:
-    MOCK_METHOD(bool, enqueue, (const ProcessMessage&), (override));
-    MOCK_METHOD(void, stop, (), (override));
+    MOCK_METHOD(void, send, (), (override));
 };
+
 class MockLogger : public ILogger {
 public:
     MOCK_METHOD(void, info, (const std::string&), (override));
@@ -25,16 +26,80 @@ public:
 };
 } // namespace
 
-TEST(TimerServiceTest, SendsTimeoutMessage) {
+TEST(TimerServiceTest, ConstructorLogsCreationWithValidArgs) {
+    StrictMock<MockLogger> logger;
     auto sender = std::make_shared<StrictMock<MockSender>>();
-    NiceMock<MockLogger> logger;
-    TimerService timer(sender, std::shared_ptr<ILogger>(&logger, [](ILogger*){}));
-    ProcessMessage m{ThreadMessageType::Timeout, false};
-    EXPECT_CALL(*sender, enqueue(testing::Field(&ProcessMessage::type_, ThreadMessageType::Timeout))).Times(1);
-    timer.start(10, m);
-    std::this_thread::sleep_for(std::chrono::milliseconds(30));
-    while (timer.active()) {
-        std::this_thread::sleep_for(std::chrono::milliseconds(10));
+
+    EXPECT_CALL(logger, info("TimerService created")).Times(1);
+    {
+        TimerService service(std::shared_ptr<ILogger>(&logger, [](ILogger*){}), 10, sender);
     }
-    timer.stop();
 }
+
+TEST(TimerServiceTest, ConstructorNoLogWhenLoggerNull) {
+    auto sender = std::make_shared<StrictMock<MockSender>>();
+
+    TimerService service(nullptr, 10, sender);
+    // Expect no crash and no log; using NiceMock to ignore
+}
+TEST(TimerServiceTest, ConstructorHandlesNegativeDuration) {
+    NiceMock<MockLogger> logger;
+    auto sender = std::make_shared<StrictMock<MockSender>>();
+    TimerService service(std::shared_ptr<ILogger>(&logger, [](ILogger*){}), -5, sender);
+    (void)service;
+}
+
+
+TEST(TimerServiceTest, StartSendsMessageAndLogs) {
+    StrictMock<MockLogger> logger;
+    auto sender = std::make_shared<StrictMock<MockSender>>();
+    TimerService service(std::shared_ptr<ILogger>(&logger, [](ILogger*){}), 10, sender);
+
+    {
+        ::testing::InSequence seq;
+        EXPECT_CALL(logger, info("TimerService started")).Times(1);
+        EXPECT_CALL(*sender, send()).Times(1);
+        EXPECT_CALL(logger, info("TimerService timeout")).Times(1);
+        EXPECT_CALL(logger, info("TimerService stopped")).Times(1);
+    }
+
+    service.start();
+    std::this_thread::sleep_for(std::chrono::milliseconds(30));
+    service.stop();
+}
+
+TEST(TimerServiceTest, StartWithoutSenderOnlyLogsStart) {
+    StrictMock<MockLogger> logger;
+    TimerService service(std::shared_ptr<ILogger>(&logger, [](ILogger*){}), 10, nullptr);
+
+    EXPECT_CALL(logger, info("TimerService started")).Times(1);
+    EXPECT_CALL(logger, info("TimerService timeout")).Times(0);
+    EXPECT_CALL(logger, info("TimerService stopped")).Times(1);
+
+    service.start();
+    std::this_thread::sleep_for(std::chrono::milliseconds(20));
+    service.stop();
+}
+
+TEST(TimerServiceTest, StartWithNullLoggerStillSends) {
+    auto sender = std::make_shared<StrictMock<MockSender>>();
+    TimerService service(nullptr, 10, sender);
+
+    EXPECT_CALL(*sender, send()).Times(1);
+
+    service.start();
+    std::this_thread::sleep_for(std::chrono::milliseconds(20));
+    service.stop();
+}
+
+
+TEST(TimerServiceTest, StopWithoutStartLogsOnce) {
+    StrictMock<MockLogger> logger;
+    auto sender = std::make_shared<StrictMock<MockSender>>();
+    TimerService service(std::shared_ptr<ILogger>(&logger, [](ILogger*){}), 10, sender);
+
+    EXPECT_CALL(logger, info("TimerService stopped")).Times(1);
+    service.stop();
+}
+
+
