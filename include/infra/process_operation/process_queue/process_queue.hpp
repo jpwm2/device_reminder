5305586b5742468ccb67cd0e2bd5e9d40af671b0
jpwm2 <118033153+jpwm2@users.noreply.github.com>
2025-08-07 22:49:14 +0900

    Refactor ProcessQueue to use IMessage

--- include/infra/process_operation/process_queue/process_queue.hpp
+++ include/infra/process_operation/process_queue/process_queue.hpp
@@ -5,7 +5,8 @@
 #include <memory>
 
 #include "infra/logger/i_logger.hpp"
-#include "infra/process_operation/message_codec/i_message_codec.hpp"
+#include "infra/message/message_codec/i_message_codec.hpp"
+#include "infra/message/i_message.hpp"
 #include "infra/process_operation/process_queue/i_process_queue.hpp"
 
 namespace device_reminder {
@@ -15,8 +16,8 @@ public:
     ProcessQueue(std::shared_ptr<ILogger> logger,
                  std::shared_ptr<IMessageCodec> codec);
 
-    void push(std::shared_ptr<IProcessMessage> msg) override;
-    std::shared_ptr<IProcessMessage> pop() override;
+    void push(std::shared_ptr<IMessage> msg) override;
+    std::shared_ptr<IMessage> pop() override;
 
 private:
     std::queue<std::vector<uint8_t>> buffer_;
