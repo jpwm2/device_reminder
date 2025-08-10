edcffbbc91999396674ebe426281568f913b5e09
jpwm2 <118033153+jpwm2@users.noreply.github.com>
2025-08-11 00:52:35 +0900

    MessageDispatcherの仕様準拠修正

--- include/infra/message/message_dispatcher.hpp
+++ include/infra/message/message_dispatcher.hpp
@@ -18,17 +18,17 @@ public:
 
 class MessageDispatcher : public IMessageDispatcher {
 public:
-    using Handler = std::function<void(std::vector<std::string>)>;
-    using HandlerMap = std::unordered_map<MessageType, Handler>;
-
-    MessageDispatcher(std::shared_ptr<ILogger> logger,
-                      HandlerMap handler_map);
+    MessageDispatcher(
+        std::shared_ptr<ILogger> logger,
+        std::unordered_map<MessageType, std::function<void(std::vector<std::string>)>>
+            handler_map);
 
     void dispatch(std::shared_ptr<IMessage> msg) override;
 
 private:
     std::shared_ptr<ILogger> logger_;
-    HandlerMap handler_map_;
+    std::unordered_map<MessageType, std::function<void(std::vector<std::string>)>>
+        handler_map_;
 };
 
 } // namespace device_reminder
