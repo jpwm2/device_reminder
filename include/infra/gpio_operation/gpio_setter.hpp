43acf565f6968632defb5bf87f0bda28756c2244
jpwm2 <118033153+jpwm2@users.noreply.github.com>
2025-08-11 15:50:20 +0900

    Refactor GPIOSetter implementation

--- include/infra/gpio_operation/gpio_setter.hpp
+++ include/infra/gpio_operation/gpio_setter.hpp
@@ -1,29 +1,44 @@
 #pragma once
 
-#include "infra/gpio_operation/gpio_setter/i_gpio_setter.hpp"
+// GPIOSetter インターフェースおよび実装
+
 #include "infra/logger/i_logger.hpp"
+#include "infra/file_loader/i_file_loader.hpp"
 
 #include <memory>
-#include <string>
 
 struct gpiod_chip;
 struct gpiod_line;
 
 namespace device_reminder {
 
+class IGPIOSetter {
+public:
+    virtual ~IGPIOSetter() = default;
+
+    virtual void write(bool is_high) = 0;
+    virtual void buzz_start(int frequency_hz) = 0;
+    virtual void buzz_stop() = 0;
+};
+
 class GPIOSetter : public IGPIOSetter {
 public:
-    GPIOSetter(std::shared_ptr<ILogger> logger,
-               int pin_no,
-               std::string chip_name = "/dev/gpiochip0");
+    GPIOSetter(std::shared_ptr<ILogger>     logger,
+               int                          pin_no,
+               std::shared_ptr<IFileLoader> loader);
     ~GPIOSetter() override;
 
-    void write(bool value) override;
+    void write(bool is_high) override;
+    void buzz_start(int frequency_hz) override;
+    void buzz_stop() override;
 
 private:
-    std::shared_ptr<ILogger> logger_;
-    gpiod_chip* chip_;
-    gpiod_line* line_;
+    std::shared_ptr<ILogger>     logger_{};
+    int                          pin_no_{};
+    std::shared_ptr<IFileLoader> loader_{};
+    gpiod_chip*                  chip_{};
+    gpiod_line*                  line_{};
 };
 
 } // namespace device_reminder
+
