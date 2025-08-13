23615b1a94b7ccbd939c8895f70a8d168dba1e2f
jpwm2 <118033153+jpwm2@users.noreply.github.com>
2025-08-13 15:45:15 +0900

    revise bluetooth handler

--- include/core/bluetooth_task/bluetooth_handler.hpp
+++ include/core/bluetooth_task/bluetooth_handler.hpp
@@ -1,34 +1,45 @@
 #pragma once
 
-#include "bluetooth_task/i_bluetooth_task.hpp"
 #include "infra/logger/i_logger.hpp"
-#include "infra/bluetooth_driver/i_bluetooth_driver.hpp"
 #include "infra/file_loader/i_file_loader.hpp"
 #include "infra/process_operation/process_sender/i_process_sender.hpp"
+#include "infra/bluetooth_driver/i_bluetooth_driver.hpp"
+#include "infra/message/message_queue.hpp"
+#include "infra/message/message.hpp"
+
 #include <memory>
+#include <string>
+#include <vector>
 
 namespace device_reminder {
 
-class BluetoothTask : public IBluetoothTask {
+class IBluetoothHandler {
 public:
-    enum class State { WaitRequest, Scanning };
+    virtual ~IBluetoothHandler() = default;
+    virtual std::vector<std::string> scan() = 0;
+};
 
-    BluetoothTask(std::shared_ptr<ILogger> logger,
-                  std::shared_ptr<IProcessSender> sender,
-                  std::shared_ptr<IFileLoader> loader,
-                  std::shared_ptr<IBluetoothDriver> driver);
-    ~BluetoothTask();
+class BluetoothHandler : public IBluetoothHandler {
+public:
+    BluetoothHandler(std::shared_ptr<ILogger> logger,
+                     std::shared_ptr<IFileLoader> loader,
+                     std::shared_ptr<IProcessSender> sender,
+                     std::shared_ptr<IBluetoothDriver> driver,
+                     std::shared_ptr<IMessageQueue> main_queue,
+                     std::shared_ptr<IMessage> success_msg,
+                     std::shared_ptr<IMessage> failure_msg);
 
-    void on_waiting(const std::vector<std::string>& payload) override;
-
-    State state() const noexcept { return state_; }
+    std::vector<std::string> scan() override;
 
 private:
-    std::shared_ptr<ILogger> logger_;
-    std::shared_ptr<IProcessSender> sender_;
-    std::shared_ptr<IFileLoader> loader_;
-    std::shared_ptr<IBluetoothDriver> driver_;
-    State state_{State::WaitRequest};
+    std::shared_ptr<ILogger> logger_{};
+    std::shared_ptr<IFileLoader> loader_{};
+    std::shared_ptr<IProcessSender> sender_{};
+    std::shared_ptr<IBluetoothDriver> driver_{};
+    std::shared_ptr<IMessageQueue> main_queue_{};
+    std::shared_ptr<IMessage> success_msg_{};
+    std::shared_ptr<IMessage> failure_msg_{};
 };
 
 } // namespace device_reminder
+
